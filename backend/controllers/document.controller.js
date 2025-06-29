2c67a7f85663d6bda171730ba888536e83c7338e
minhchuong32 <chuongminh3225@gmail.com>
2025-06-29 16:38:35 +0700

    create document.controller.js

---
+++ backend/controllers/document.controller.js
@@ -0,0 +1,37 @@
+import Document from "../models/Document.js";
+
+const uploadDocument = async (req, res) => {
+  const { title, description, subject, fileUrl } = req.body;
+  const userId = req.user.id;
+
+  if (!title || !description || !subject || !fileUrl) {
+    return res
+      .status(400)
+      .json({ message: "Vui lòng nhập đầy đủ thông tin tài liệu" });
+  }
+
+  try {
+    const doc = new Document({
+      title,
+      description,
+      subject,
+      fileUrl,
+      uploadedBy: userId,
+    });
+    await doc.save();
+    res.status(201).json({ message: "Tải tài liệu thành công", doc });
+  } catch (err) {
+    res.status(500).json({ message: "Lỗi server khi upload" });
+  }
+};
+
+const getAllDocuments = async (req, res) => {
+  try {
+    const docs = await Document.find().populate("uploadedBy", "name email");
+    res.json(docs);
+  } catch (err) {
+    res.status(500).json({ message: "Lỗi server" });
+  }
+};
+
+export { uploadDocument, getAllDocuments };
