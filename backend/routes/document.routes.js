5ee4de4be2d49f36f678688acf8746ad0388ccf1
minhchuong32 <chuongminh3225@gmail.com>
2025-06-29 16:40:18 +0700

    add routes

---
+++ backend/routes/document.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  uploadDocument,
+  getAllDocuments,
+} from "../controllers/document.controller.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/upload", protect, uploadDocument);
+router.get("/", getAllDocuments);
+
+export default router;
